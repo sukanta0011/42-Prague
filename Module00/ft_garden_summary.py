1207b32f0a3c77594acc07f32da2dc78f03cbdd6
sudas <das.sukanta2@gmail.com>
2025-12-18 16:02:07 +0100

    42 header removed

--- Module00/ft_garden_summary.py
+++ Module00/ft_garden_summary.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_summary.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: sudas <sudas@student.42prague.com>         +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/09 14:23:52 by sudas             #+#    #+#              #
-#    Updated: 2025/12/09 14:23:52 by sudas            ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_garden_summary():
     grdn_name = input("Enter garden name: ")
     total_plants = input("Enter number of plants: ")
